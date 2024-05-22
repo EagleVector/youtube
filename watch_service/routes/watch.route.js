9a43176420f7afd71b98c2d2a4734515f53569d5
EagleVector <kumarsubhambili3@gmail.com>
2024-05-23 00:05:49 +0530

    Getting all the uploaded video metadata from postgres for watch service backend

--- watch_service/routes/watch.route.js
+++ watch_service/routes/watch.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import getAllVideos from '../controllers/home.controller.js';
 import watchVideo from '../controllers/watch.controller.js';
 
 const watchRouter = express.Router();
 
 watchRouter.get('/', watchVideo);
+watchRouter.get('/home', getAllVideos)
+
 
 export default watchRouter;
