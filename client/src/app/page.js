7534b0b90c7c37795a6a817e70c31fbb43a3d745
EagleVector <kumarsubhambili3@gmail.com>
2024-05-23 01:24:23 +0530

    Rendering videos from S3 to frontend

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -2,11 +2,12 @@ import React from 'react';
 import UploadForm from './components/uploadForm';
 import Auth from './pages/Auth';
 import Room from './pages/Room';
+import YoutubeHome from './pages/YoutubeHome';
 
 export default function Home() {
 	return (
 		<div>
-			<UploadForm />
+			<YoutubeHome />
 		</div>
 	);
 }
